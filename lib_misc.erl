54cd4c2d26c9500f369190e2b5f4e9aeecc13ccb
Kerem Bozdas <krmbzds.github@gmail.com>
2019-02-04 21:27:11 +0300

    Add miscellaneous methods

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([for/3]).
+-export([for/3, qsort/1, pythag/1, perms/1]).
 
 % Creating our own control flows
 
@@ -13,3 +13,29 @@ for(Min, Max, F) -> [F(Min)|for(Min+1, Max, F)].
 % Square = fun(X) -> X * X end.
 % lib_misc:for(1, 10, Square).
 % [1,4,9,16,25,36,49,64,81,100]
+
+qsort([]) -> [];
+qsort([Pivot|T]) ->
+        qsort([X || X <- T, X < Pivot])
+        ++ [Pivot] ++
+        qsort([X || X <- T, X >= Pivot]).
+
+% L=[23,6,2,9,27,400,78,45,61,82,14].
+% lib_misc:qsort(L).
+
+pythag(N) ->
+  [ {A, B, C} ||
+    A <- lists:seq(1, N),
+    B <- lists:seq(1, N),
+    C <- lists:seq(1, N),
+    A + B + C =< N,
+    A*A + B*B =:= C*C
+  ].
+
+% lib_misc:pythag(16).
+% [{3,4,5},{4,3,5}]
+% lib_misc:pythag(30).
+% [{3,4,5},{4,3,5},{5,12,13},{6,8,10},{8,6,10},{12,5,13}]
+
+perms([]) -> [[]];
+perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
