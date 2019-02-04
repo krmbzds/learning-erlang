a8d995a3fcdf597ce8d2d51547a9c7a63bc8d16b
Kerem Bozdas <krmbzds.github@gmail.com>
2019-02-04 21:44:47 +0300

    Add odds and evens example

--- lib_misc.erl
+++ lib_misc.erl
@@ -16,9 +16,9 @@ for(Min, Max, F) -> [F(Min)|for(Min+1, Max, F)].
 
 qsort([]) -> [];
 qsort([Pivot|T]) ->
-        qsort([X || X <- T, X < Pivot])
-        ++ [Pivot] ++
-        qsort([X || X <- T, X >= Pivot]).
+  qsort([X || X <- T, X < Pivot])
+  ++ [Pivot] ++
+  qsort([X || X <- T, X >= Pivot]).
 
 % L=[23,6,2,9,27,400,78,45,61,82,14].
 % lib_misc:qsort(L).
@@ -39,3 +39,21 @@ pythag(N) ->
 
 perms([]) -> [[]];
 perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
+% This implementation goes over the list twice
+odds_and_evens1(L) ->
+  Odds  = [X || X <- L, (X rem 2) =:= 1],
+  Evens = [X || X <- L, (X rem 2) =:= 0],
+  {Odds, Evens}.
+
+% This implementation goes over the list once
+odds_and_evens2(L) ->
+  odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+  case (H rem 2) of
+    1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+    0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+  end;
+odds_and_evens_acc([], Odds, Evens) ->
+  {lists:reverse(Odds), lists:reverse(Evens)}.
